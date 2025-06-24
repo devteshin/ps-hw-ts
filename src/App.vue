45600af47a1b3e39b9f5c5e2839e0b99fe443c4f
DmitryEvteshin <dmitry.evteshin@yandex.ru>
2025-06-24 10:03:00 +0400

    start

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="app">
+    <div class="start-page">
+      Добро пожаловать
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .app {
+    display: flex;
+  }
+  .start-page {
+    color: white;
+  }
+
+</style>
